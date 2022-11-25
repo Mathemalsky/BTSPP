cc647c0d113b34804790b7438a13232fb7b66247
Jurek Rostalsky <jurek.rostalsky@ovgu.de>
2022-11-25 14:05:49 +0100

    add basic functions and fix glfw linking

--- implementation/src/main.cpp
+++ implementation/src/main.cpp
@@ -1,6 +1,18 @@
+#include <cstdio>
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+// error callback function which prints glfw errors in case they arise
+static void glfw_error_callback(int error, const char* description) {
+  fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+}
+
 int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
+  // set error colback function
+  glfwSetErrorCallback(glfw_error_callback);
+  if (!glfwInit()) {
+    return -1;
+  }
   return 0;
 }
