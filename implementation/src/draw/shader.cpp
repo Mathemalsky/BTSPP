9dee4f852c8e4a9b637b91e8fd98a153b4219cab
Jurek Rostalsky <jurek.rostalsky@ovgu.de>
2022-11-29 12:07:00 +0100

    add geometry shader

--- implementation/src/draw/shader.cpp
+++ implementation/src/draw/shader.cpp
@@ -5,22 +5,41 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-constexpr const char vertexShaderSource[] =
-  "#version 440 core\n"
-  "layout(location = 0) in vec2 vertexPosition;\n"
-  "void main()\n"
-  "{\n"
-  "   gl_Position = vec4(vertexPosition, 0.0, 1.0);\n"
-  "}\0";
+constexpr const char vertexShaderSource[] = R"glsl(
+  #version 440 core
+  layout(location = 0) in vec2 vertexPosition;
 
-constexpr const char fragmentShaderSource[] =
-  "#version 440 core\n"
-  "out vec3 color;\n"
-  "void main(){\n"
-  "  color = vec3(1,0,0);\n"
-  "}\0";
+  void main() {
+    gl_Position = vec4(vertexPosition, 0.0, 1.0);
+  }
+)glsl";
 
-GLuint compileShader(const GLenum shaderType, const char* shaderSource) {
+constexpr const char geometryShaderSource[] = R"glsl(
+  #version 440 core
+  layout(points) in;
+  layout(line_strip, max_vertices = 2) out;
+
+  void main() {
+    gl_Position = gl_in[0].gl_Position + vec4(-0.1, 0.0, 0.0, 0.0);
+    EmitVertex();
+
+    gl_Position = gl_in[0].gl_Position + vec4(0.1, 0.0, 0.0, 0.0);
+    EmitVertex();
+
+    EndPrimitive();
+}
+)glsl";
+
+constexpr const char fragmentShaderSource[] = R"glsl(
+  #version 440 core
+  out vec3 color;
+
+  void main() {
+    color = vec3(1,0,0);
+  }
+)glsl";
+
+GLuint compileShader(const GLenum shaderType, const GLchar* shaderSource) {
   const GLuint shader = glCreateShader(shaderType);
   glShaderSource(shader, 1, &shaderSource, nullptr);
   glCompileShader(shader);
@@ -38,8 +57,10 @@ GLuint compileShader(const GLenum shaderType, const char* shaderSource) {
 GLuint linkShaders() {
   const GLuint shaderProgram  = glCreateProgram();
   const GLuint vertexShader   = compileShader(GL_VERTEX_SHADER, vertexShaderSource);
+  const GLuint geometryShader = compileShader(GL_GEOMETRY_SHADER, geometryShaderSource);
   const GLuint fragmentShader = compileShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
   glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, geometryShader);
   glAttachShader(shaderProgram, fragmentShader);
   glLinkProgram(shaderProgram);
 
@@ -56,6 +77,7 @@ GLuint linkShaders() {
 
   glDeleteShader(vertexShader);
   glDeleteShader(fragmentShader);
+  glDeleteShader(geometryShader);
 
   return shaderProgram;
 }
