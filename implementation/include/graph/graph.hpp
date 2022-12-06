b76cf684f601119857183934eff75885368f520e
Jurek Rostalsky <jurek.rostalsky@ovgu.de>
2022-12-06 15:33:09 +0100

    add implementation of check for connectedness

--- implementation/include/graph/graph.hpp
+++ implementation/include/graph/graph.hpp
@@ -1,40 +1,28 @@
 #pragma once
 
+#include <iostream>
 #include <vector>
 
 #include <Eigen/SparseCore>
 
 #include "graph/geometry.hpp"
 
-class Node {
-public:
-  Node() = default;
-  Node(unsigned int v) : pIndex(v) {}
-
-  ~Node() = default;
-
-  void operator=(unsigned int v) { pIndex = v; }
-  bool operator<(const Node other) const { return pIndex < other.pIndex; }
-  bool operator>(const Node other) const { return pIndex > other.pIndex; }
-  unsigned int& index() { return pIndex; }
-  unsigned int index() const { return pIndex; }
-
-private:
-  unsigned int pIndex;
-};
-
 class Graph {
 public:
   Graph()  = default;
   ~Graph() = default;
 
-  virtual double distance(const Node& u, const Node& v) = 0;
-  virtual bool adjacent(const Node& u, const Node& v)   = 0;
+  Graph(const size_t numberOfNodes) : pNumberOfNodes(numberOfNodes) {}
 
-  unsigned int numberOfNodes() { return pNumberOfNodes; }
+  virtual double distance(const size_t& u, const size_t& v) = 0;
+  virtual bool adjacent(const size_t& u, const size_t& v)   = 0;
+  virtual bool connected() const                            = 0;
+  // virtual bool connected() const = 0;
+
+  size_t numberOfNodes() const { return pNumberOfNodes; }
 
 protected:
-  unsigned int pNumberOfNodes;
+  size_t pNumberOfNodes;
 };
 
 class CompleteGraph : public Graph {
@@ -42,7 +30,8 @@ public:
   CompleteGraph()  = default;
   ~CompleteGraph() = default;
 
-  bool adjacent([[maybe_unused]] const Node& u, [[maybe_unused]] const Node& v) override { return true; }
+  bool adjacent([[maybe_unused]] const size_t& u, [[maybe_unused]] const size_t& v) override { return true; }
+  bool connected() const override { return true; }
 };
 
 class Euclidean : public CompleteGraph {
@@ -52,8 +41,8 @@ public:
 
   ~Euclidean() = default;
 
-  double distance(const Node& u, const Node& v) override { return dist(pPositions[u.index()], pPositions[v.index()]); }
-  Point2D position(const Node& v) const { return pPositions[v.index()]; }
+  double distance(const size_t& u, const size_t& v) override { return dist(pPositions[u], pPositions[v]); }
+  Point2D position(const size_t& v) const { return pPositions[v]; }
   const std::vector<Point2D>& allPositions() const { return pPositions; }
   Point2D* pointer() { return &pPositions[0]; }
 
@@ -69,8 +58,8 @@ public:
   double operator()() const { return pCost; }
   void operator=(const double cost) { pCost = cost; }
 
-  EdgeCost operator+(const EdgeCost other) { return std::min(pCost, other.pCost); }
-  EdgeCost operator*(const EdgeCost other) { return pCost + other.pCost; }
+  EdgeCost operator+(const EdgeCost other) const { return std::min(pCost, other.pCost); }
+  EdgeCost operator*(const EdgeCost other) const { return pCost + other.pCost; }
 
 private:
   double pCost;
@@ -102,29 +91,35 @@ class SimpleGraph : public Graph {
 public:
   SimpleGraph() = default;
   SimpleGraph(const size_t numberOfNodes)
-    : pAdjacencyMatrix(Eigen::SparseMatrix<EdgeCost>(numberOfNodes, numberOfNodes)) {}
+    : Graph(numberOfNodes), pAdjacencyMatrix(Eigen::SparseMatrix<EdgeCost>(numberOfNodes, numberOfNodes)) {}
+  SimpleGraph(const std::vector<Eigen::Triplet<EdgeCost>>& tripletList) : Graph(tripletList.size()) {
+    pAdjacencyMatrix.setFromTriplets(tripletList.begin(), tripletList.end());
+  }
 
   ~SimpleGraph();
-  // constructor from 3 vectors
 
   // removal of a column and a row
 
-  virtual void addEdge(const Node out, const Node in, const EdgeCost edge) = 0;
+  virtual void addEdge(const size_t out, const size_t in, const EdgeCost edge) = 0;
 
 protected:
   Eigen::SparseMatrix<EdgeCost> pAdjacencyMatrix;
+  void warnLoop(const size_t node) { std::cerr << "[Graph] Warning: inserting a loop on node " << node << std::endl; }
 };
 
 class UndirectedGraph : public SimpleGraph {
-  void addEdge(const Node out, const Node in, const EdgeCost edge) override {
-    if (out > in) {
-      pAdjacencyMatrix.insert(out.index(), in.index()) = edge;
-    }
-    else if (out < in) {
-    }
-    else {
-    }
+  void addEdge(const size_t out, const size_t in, const EdgeCost edge) override {
+    out > in ? pAdjacencyMatrix.insert(out, in) = edge : pAdjacencyMatrix.insert(in, out);
   }
+
+  bool connected() const override;
 };
 
-class Digraph : public SimpleGraph {};
+class Digraph : public SimpleGraph {
+  void addEdge(size_t out, size_t in, const EdgeCost edge) override {
+    if (out == in) {
+      warnLoop(out);
+    }
+    pAdjacencyMatrix.insert(out, in) = edge;
+  }
+};
