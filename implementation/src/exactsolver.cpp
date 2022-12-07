fae5b515cb0e24865ff88c82d556856764bf62c4
Jurek Rostalsky <jurek.rostalsky@ovgu.de>
2022-12-07 15:13:15 +0100

    continue implementing miller-tucker-zemlin

--- implementation/src/exactsolver.cpp
+++ implementation/src/exactsolver.cpp
@@ -14,7 +14,7 @@ void solveExact(const Euclidean* euclidean) {
 
   HighsModel model;
   model.lp_.num_col_ = numberOfNodes * numberOfNodes;
-  model.lp_.num_row_ = 2 * numberOfNodes + (numberOfNodes - 1) * (numberOfNodes - 2) / 2;
+  model.lp_.num_row_ = 2 * numberOfNodes + (numberOfNodes - 1) * (numberOfNodes - 2);
   model.lp_.sense_   = ObjSense::kMinimize;
   model.lp_.offset_  = 0;  // offset has no effect on optimization
 
@@ -32,17 +32,17 @@ void solveExact(const Euclidean* euclidean) {
   model.lp_.col_lower_ = std::vector(model.lp_.num_col_, 0.0);  // set lower bound to zero
 
   // iterate over all variables
-  const double BIG_M = numberOfNodes;  // can probably be shrinked to numberOfNodes -1
+  const double bigM = numberOfNodes;  // can probably be shrinked to numberOfNodes -1
   model.lp_.col_upper_.resize(model.lp_.num_col_);
   model.lp_.integrality_.resize(model.lp_.num_col_);
   for (size_t j = 0; j < numberOfNodes; ++j) {
     for (size_t i = j + 1; i < numberOfNodes; ++i) {
-      model.lp_.col_upper_[j * numberOfNodes + i]   = 1.0;
-      model.lp_.col_upper_[i * numberOfNodes + j]   = 1.0;  // exploiting symmetry
-      model.lp_.integrality_[j * numberOfNodes + i] = HighsVarType::kInteger;
+      model.lp_.col_upper_[j * numberOfNodes + i]   = 1.0;                     // bound by 1
+      model.lp_.col_upper_[i * numberOfNodes + j]   = 1.0;                     // exploiting symmetry
+      model.lp_.integrality_[j * numberOfNodes + i] = HighsVarType::kInteger;  // constrain x_ij to be \in {0,1}
       model.lp_.integrality_[i * numberOfNodes + j] = HighsVarType::kInteger;  // exploiting symmetry
     }
-    model.lp_.col_upper_[j * numberOfNodes + j]   = BIG_M;
+    model.lp_.col_upper_[j * numberOfNodes + j]   = bigM;
     model.lp_.integrality_[j * numberOfNodes + j] = HighsVarType::kContinuous;
   }
 
@@ -50,10 +50,26 @@ void solveExact(const Euclidean* euclidean) {
   model.lp_.row_lower_.resize(model.lp_.num_row_);
   model.lp_.row_upper_.resize(model.lp_.num_row_);
   for (size_t i = 0; i < 2 * numberOfNodes; ++i) {
+    // fix the sum over the x_i to 1
     model.lp_.row_lower_[i] = 1.0;
     model.lp_.row_upper_[i] = 1.0;
   }
+  const double p = numberOfNodes;
+  for (size_t j = 0; j < numberOfNodes - 1; ++j) {
+    for (size_t i = j + 1; i < numberOfNodes - 1; ++i) {
+      model.lp_.row_upper_[2 * numberOfNodes + j * (numberOfNodes - 1) + i] = p - 1;
+      model.lp_.row_upper_[2 * numberOfNodes + i * (numberOfNodes - 1) + j] = p - 1;  // exploiting symmetry
+    }
+  }
 
-  model.lp_.row_lower_ = std::vector(model.lp_.num_row_, 0.0);  // set lower bound to zero // rework
-  model.lp_.row_upper_ = std::vector(model.lp_.num_row_, 0.0);  // set lower bound to zero // rework
+  model.lp_.row_lower_ = std::vector(model.lp_.num_row_, -1.0e32);  // set lower bound to -infinty
+
+  // construct matrix A
+  // DEFICIENT
+  model.lp_.a_matrix_.format_ = MatrixFormat::kColwise;         // use column compressed storage order
+  for (size_t j = 0; j < (size_t) model.lp_.num_col_; ++j) {    // iterate through columns
+    for (size_t i = 0; i < (size_t) model.lp_.num_row_; ++i) {  // iterate through rows
+    }
+  }
+  // model.lp_.a_matrix_.start_;
 }
