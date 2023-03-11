9e77efb7270e4692458068f644b1611129e12bcc
Jurek Rostalsky <jurek.rostalsky@ovgu.de>
2023-03-11 16:53:48 +0100

    add cmake option to leave out visualization

--- implementation/src/main.cpp
+++ implementation/src/main.cpp
@@ -8,5 +8,9 @@
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
   graph::Euclidean euclidean = generateEuclideanDistanceGraph(std::atoi(argv[1]));
+
+#if (VISUALIZATION)
   return visualize(euclidean);
+#else
+#endif
 }
